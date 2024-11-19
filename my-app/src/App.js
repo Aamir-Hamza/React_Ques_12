af3124722a7173172f4174d0c11521ed31d7825f
Aamiramza <ek939774@gmail.com>
2024-11-19 15:15:08 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    <ul>
+      <li>React </li>
+      <li>JavaScript </li>
+      <li>Css </li>
+    </ul>
+    
+    
+    
+    </>
+  )
+}
+
+export default App
